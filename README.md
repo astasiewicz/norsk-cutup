61bf08df36b34527fe2c86578a6049636596dda2
Arek <astasiewicz@squiz.pl>
2018-01-13 18:05:15 +0100

    several last adjustments

---
+++ README.md
@@ -0,0 +1,22 @@
+# Food List Cutup
+
+Simple cutup based on gulp-webapp kit.  
+
+# Installation
+
+`git clone https://github.com/astasiewicz/norsk-cutup.git`
+
+`cd norsk-cutup`
+
+`npm install`
+
+`bower install`
+
+`gulp serve`
+
+Application will be served from http://localhost:9000/
+
+# Creating a build (dist)
+
+`gulp build`
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,18 @@
+'use strict';
 $(function() {
+
+    /* Mobile Menu Toggle */
     var $menuToggleBtn = $('#menu-toggle');
     var $navMenu = $('#nav-menu');
 
-    $menuToggleBtn.on('click', function(ev) {
-        ev.stopImmediatePropagation();
+    $menuToggleBtn.on('click', function() {
+        // avoid multiple event fires
+        if ($navMenu.is(':animated')) { 
+            return false; 
+        }
         $menuToggleBtn.toggleClass('active');
         $navMenu.slideToggle(300);
-
+        
         if($menuToggleBtn.hasClass('active')) {
             $menuToggleBtn.find('i').removeClass('fa-bars').addClass('fa-remove');
         }
