2599456e23be4275dafee689c5afe61db58e2a03
Arek <astasiewicz@squiz.pl>
2018-01-13 17:26:23 +0100

    main items list + footer HTML/CSS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    var $menuToggleBtn = $('#menu-toggle');
+    var $navMenu = $('#nav-menu');
+
+    $menuToggleBtn.on('click', function(ev) {
+        ev.stopImmediatePropagation();
+        $menuToggleBtn.toggleClass('active');
+        $navMenu.slideToggle(300);
+
+        if($menuToggleBtn.hasClass('active')) {
+            $menuToggleBtn.find('i').removeClass('fa-bars').addClass('fa-remove');
+        }
+        else {
+            $menuToggleBtn.find('i').removeClass('fa-remove').addClass('fa-bars');
+        }
+    });
+})
